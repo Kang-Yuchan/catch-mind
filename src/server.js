7fe696b1ca9ebe6f03360a3afc7e31b279450039
Kang.Yuchan <ikimaku0517@gmail.com>
2019-12-31 05:43:11 +0900

    Setup Socket IO

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
-import express from "express";
-import {join} from "path";
-import socketIO from "socket.io";
-
+import express from 'express';
+import { join } from 'path';
+import socketIO from 'socket.io';
 
 const app = express();
 const PORT = 4000;
-app.set("view engine", "pug");
-app.set("views", join(__dirname, "views"));
-app.use(express.static(join(__dirname, "static")));
-app.get("/", (req, res) => res.render("home"));
+app.set('view engine', 'pug');
+app.set('views', join(__dirname, 'views'));
+app.use(express.static(join(__dirname, 'static')));
+app.get('/', (req, res) => res.render('home'));
 
-const handleListening = () => 
-    console.log(`Server running: http://localhost:${PORT}`);
+const handleListening = () => console.log(`Server running: http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening);
+const server = app.listen(PORT, handleListening);
+
+const io = socketIO(server);
