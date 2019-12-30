9cab18fe3faad5b59dca8fb318c3e4b32598fee2
Kang.Yuchan <ikimaku0517@gmail.com>
2019-12-31 05:27:42 +0900

    Serve SetUp

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {join} from "path";
+import socketIO from "socket.io";
+
+
+const app = express();
+const PORT = 4000;
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(express.static(join(__dirname, "static")));
+app.get("/", (req, res) => res.render("home"));
+
+const handleListening = () => 
+    console.log(`Server running: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
